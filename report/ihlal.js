4ed2757ca500a14511b2092f19b7dda8efa4e8c1
yusufbayrakdar <sabri.bayrakdar@boun.edu.tr>
2020-01-11 04:53:16 +0300

    Sheet 2 OK

--- report/ihlal.js
+++ report/ihlal.js
@@ -63,7 +63,11 @@ function getRow(number) {
 }
 
 function IhlalRapor() {
-  table2Offset = 0;
+  let table2Offset = 0;
+  let table1Length = 3;
+
+  let Sum1 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let Sum2 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   for (
     let i = 0;
     i < Object.keys(Ihlal_Raporu.sheets[0].subheaders).length;
@@ -81,6 +85,7 @@ function IhlalRapor() {
       value: Ihlal_Raporu.sheets[0].values[i]
     };
   }
+  table1Length++;
   for (let i = 65; i < 72; i++) {
     let cell = String.fromCharCode(i) + 4;
     content.sheets[1].cells[cell] = {
@@ -98,17 +103,81 @@ function IhlalRapor() {
       content.sheets[1].cells[cell] = {
         vertical: "center",
         fontSize: 10,
-        value: Ihlal_Raporu.sheets[1].tables[0].items[0].values[i - 65]
+        value: Ihlal_Raporu.sheets[1].tables[0].items[j].values[i - 65]
       };
       table2Offset = j + 8;
+      if (i - 65 > 2) {
+        let number = Number(
+          Ihlal_Raporu.sheets[1].tables[0].items[j].values[i - 65]
+        );
+        Sum1[i - 65] += number;
+        cell = String.fromCharCode(i) + (j + 6);
+        content.sheets[1].cells[cell] = {
+          vertical: "center",
+          fontSize: 10,
+          value: Sum1[i - 65]
+        };
+      }
     }
   }
+  table1Length++;
+  cell = "A" + (table2Offset - 2);
+  content.sheets[1].cells[cell] = {
+    bold: true,
+    vertical: "center",
+    fontSize: 10,
+    value: "Toplam :"
+  };
   cell = `A${table2Offset}:G${table2Offset}`;
   content.sheets[1].cells[cell] = {
     bold: true,
     vertical: "center",
     fontSize: 10,
     value: Ihlal_Raporu.sheets[1].tables[1].name
+  };
+  table1Length += 2;
+  console.log("TCL: table1Length", table1Length);
+
+  for (let i = 65; i < 72; i++) {
+    cell = String.fromCharCode(i) + (table2Offset + 1);
+    content.sheets[1].cells[cell] = {
+      bold: true,
+      vertical: "center",
+      fontSize: 10,
+      value: Ihlal_Raporu.sheets[1].tables[1].headers[i - 65]
+    };
+    for (
+      let j = 0;
+      j < Object.keys(Ihlal_Raporu.sheets[1].tables[1].items).length;
+      j++
+    ) {
+      cell = String.fromCharCode(i) + (table2Offset + 2 + j);
+      content.sheets[1].cells[cell] = {
+        vertical: "center",
+        fontSize: 10,
+        value: Ihlal_Raporu.sheets[1].tables[1].items[j].values[i - 65]
+      };
+      if (i - 65 > 2) {
+        let number = Number(
+          Ihlal_Raporu.sheets[1].tables[1].items[j].values[i - 65]
+        );
+        Sum2[i - 65] += number;
+        cell = String.fromCharCode(i) + (j + table2Offset + 3);
+        content.sheets[1].cells[cell] = {
+          vertical: "center",
+          fontSize: 10,
+          value: Sum2[i - 65]
+        };
+      }
+    }
+  }
+  cell = "A" + (table2Offset + 4);
+  console.log("TCL: cell", cell);
+  content.sheets[1].cells[cell] = {
+    bold: true,
+    vertical: "center",
+    fontSize: 10,
+    value: "Toplam :"
   };
   createExcel(content);
   console.log("TCL: content", content);
