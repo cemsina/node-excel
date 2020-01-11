74d9215e53ce3d0a0e1add05a1e9652437983922
yusufbayrakdar <sabri.bayrakdar@boun.edu.tr>
2020-01-11 03:22:56 +0300

    Update

--- report/ihlal.js
+++ report/ihlal.js
@@ -35,11 +35,35 @@ let content = {
       cells: {
         "A1:B1": { value: Ihlal_Raporu.sheets[0].name }
       }
+    },
+    {
+      name: Ihlal_Raporu.sheets[1].name,
+      cells: {
+        F1: { value: "Tarih" },
+        G1: { value: Ihlal_Raporu.sheets[1].date },
+        "A3:G3": {
+          value: Ihlal_Raporu.sheets[1].tables[0].name,
+          bold: true,
+          vertical: "center",
+          fontSize: 10
+        }
+      }
     }
   ]
 };
 
+function getRow(number) {
+  switch (number) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+}
+
 function IhlalRapor() {
+  table2Offset = 0;
   for (
     let i = 0;
     i < Object.keys(Ihlal_Raporu.sheets[0].subheaders).length;
@@ -57,6 +81,35 @@ function IhlalRapor() {
       value: Ihlal_Raporu.sheets[0].values[i]
     };
   }
+  for (let i = 65; i < 72; i++) {
+    let cell = String.fromCharCode(i) + 4;
+    content.sheets[1].cells[cell] = {
+      bold: true,
+      vertical: "center",
+      fontSize: 10,
+      value: Ihlal_Raporu.sheets[1].tables[0].headers[i - 65]
+    };
+    for (
+      let j = 0;
+      j < Object.keys(Ihlal_Raporu.sheets[1].tables[0].items).length;
+      j++
+    ) {
+      cell = String.fromCharCode(i) + (j + 5);
+      content.sheets[1].cells[cell] = {
+        vertical: "center",
+        fontSize: 10,
+        value: Ihlal_Raporu.sheets[1].tables[0].items[0].values[i - 65]
+      };
+      table2Offset = j + 8;
+    }
+  }
+  cell = `A${table2Offset}:G${table2Offset}`;
+  content.sheets[1].cells[cell] = {
+    bold: true,
+    vertical: "center",
+    fontSize: 10,
+    value: Ihlal_Raporu.sheets[1].tables[1].name
+  };
   createExcel(content);
   console.log("TCL: content", content);
 }
